4f19017769643babeca8be4aa9d1556360fbb365
Alexey Gulyaev <sargonmeister@gmail.com>
2022-10-04 14:07:58 +0300

    Initial Commit

---
+++ MVPLevelOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPLevelOne
+//
+//  Created by Алексей Гуляев on 04.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
